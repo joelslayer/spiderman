5afcdcbcda7e8b9cf7daded6416ae3ecd3d23ee0
Joel Gomes dos Santos <j.k.slayer@hotmail.com>
2018-07-31 08:42:40 -0300

    Criacao da pagina index, fundo e barra de menu no topo da página (logo e menu)

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, user-scalable=no" />
+	
+	<link rel="stylesheet" type="text/css" href="assets/css/normalize.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/css/template.css"/>
+
+	<script type="text/javascript" src="assets/js/script.js"></script>
+	
+	<title>SPIDERMAN</title>
+
+</head>
+<body>
+	<section class="navbar justify-center">
+		<div class="container align-items-center space-between">
+			<div class="logo">
+				<img src="assets/images/logo.png"/>
+			</div>
+			<nav class="menu">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Developer</a></li>
+					<li><a href="">O Game</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+</body>
+</html>
