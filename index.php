f95faecaf0a7beca90e0d3dd91653763442bb43b
Joel Gomes dos Santos <j.k.slayer@hotmail.com>
2018-08-01 08:28:31 -0300

    nova funcionalidade x

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
 	<meta charset="utf-8"/>
-	<meta name="viewport" content="width=device-width, user-scalable=1" />
+	
 	
 	<link rel="stylesheet" type="text/css" href="assets/css/normalize.css"/>
 	<link rel="stylesheet" type="text/css" href="assets/css/template.css"/>
